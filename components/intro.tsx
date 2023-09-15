5bd9618ff0f5aa7446ab1f0969e5e5fd52f37b36
arm8tron <anirudh.makuluri@gmail.com>
2023-09-15 18:36:12 +0530

    Changed profile img

--- components/intro.tsx
+++ components/intro.tsx
@@ -9,6 +9,7 @@ import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import { useSectionInView } from "@/lib/hooks";
 import { useActiveSectionContext } from "@/context/active-section-context";
+import profileImg from "@/public/bitmoji.png";
 
 export default function Intro() {
   const { ref } = useSectionInView("Home", 0.5);
@@ -31,7 +32,7 @@ export default function Intro() {
             }}
           >
             <Image
-              src="/../public/bitmoji.png"
+              src={profileImg}
               alt="Anirudh portrait"
               width="192"
               height="192"
