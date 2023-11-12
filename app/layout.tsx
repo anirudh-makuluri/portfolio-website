75dd3b3ccb51ed02c7f47bcda30ac841d0e32a5b
arm8tron <anirudh.makuluri@gmail.com>
2023-11-12 23:18:02 +0530

    Added web analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import { Analytics } from '@vercel/analytics/react';
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -38,6 +40,7 @@ export default function RootLayout({
             <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
+		<Analytics/>
       </body>
     </html>
   );
